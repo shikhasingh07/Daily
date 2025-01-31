62fc89c4f12dfb6b2546a741cc945bb294bcd227
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-01-31 10:45:59 +0530

    424 :- Longest Repeating Character Replacement

--- DSA/SlidingWindow/424.js
+++ DSA/SlidingWindow/424.js
@@ -1,9 +1,29 @@
 var characterReplacement = function(s, k) {
-    if(str.length === 0) {
+    if(s.length === 0) {
         return -1; 
     }
 
-    
+    let left = 0 ;
+    let maxLen = 0 ;
+    let maxfreq = 0; 
+    let freq = {}; 
+
+   for(let right = 0 ; right < s.length ; right ++){
+
+      freq[s[right]] = (freq[s[right]] || 0) + 1; 
+
+      maxfreq = Math.max(freq[s[right]] , maxfreq); 
+
+      if((right - left + 1) - maxfreq > k){
+
+        freq[s[left]]--; 
+        left ++; 
+      }
+ 
+      maxLen = Math.max(maxLen , (right-left+1)); 
+   }
+
+   return maxLen;
 };
 
-characterReplacement("ABAB",2)
+console.log(characterReplacement("ABAB",2))
