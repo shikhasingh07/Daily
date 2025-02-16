1d30b1ccd16c42a3745970b26312aaa2cad69df6
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-02-16 21:55:03 +0530

    Baseball Game

--- DSA/Stack/682.js
+++ DSA/Stack/682.js
@@ -1,3 +1,18 @@
-var calPoints = function(operations) {
-    
+var calPoints = function (operations) {
+  let stack = [];
+
+  for (let item of operations) {
+    if (item === "C") {
+      stack.pop();
+    } else if (item === "D") {
+      stack.push(stack[stack.length - 1] * 2);
+    } else if (item === "+") {
+      stack.push(stack[stack.length - 1] + stack[stack.length - 2]);
+    } else {
+      stack.push(parseInt(item));
+    }
+  }
+  return stack.reduce((sum, num) => sum + num, 0);
 };
+
+console.log(calPoints(["5", "2", "C", "D", "+"]));
