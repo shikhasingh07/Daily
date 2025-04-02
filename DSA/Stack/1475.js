6bf9ac1f07e36ee7610ddae0f40458cebba72fe5
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-04-02 22:03:42 +0530

    1475. Final Prices With a Special Discount in a Shop

---
+++ DSA/Stack/1475.js
@@ -0,0 +1,14 @@
+var finalPrices = function (prices) {
+  let result = [...prices];
+  let stack = [];
+  for (let i = 0 ; i < prices.length ;i++) {
+    while (stack.length > 0 && prices[i] <= prices[stack[stack.length - 1]] ) {
+      let ans = stack.pop();
+      result[ans] -= prices[i];
+    }
+    stack.push(i);
+  }
+  return result;
+};
+
+console.log(finalPrices([8, 4, 6, 2, 3]));
