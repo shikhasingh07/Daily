f19f1800842c92f00a0cf98cb96557098b26642a
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-04-26 17:46:28 +0530

    895. Maximum Frequency Stack

--- DSA/Stack/895.js
+++ DSA/Stack/895.js
@@ -1,6 +1,6 @@
 var FreqStack = function () {
-  this.arr = [];
-  this.group = []
+  this.max = 0;
+  this.group = [];
   this.map = {};
 };
 
@@ -14,27 +14,27 @@ FreqStack.prototype.push = function (val) {
   } else {
     this.map[val] = 1;
   }
-  this.arr.push({val : val , freq : this.map[val] });
+  const freq = this.map[val];
+  //   means
+//   {3:[5] , 4:[]}
+  if(!this.group[freq]){
+    this.group[freq] = []
+  }
+  this.group[freq].push(val);
+  this.max = Math.max(freq , this.max); 
 };
 
 /**
  * @return {number}
  */
 FreqStack.prototype.pop = function () {
-    let max = 0 ; 
-    for(let key in this.map){
-        max = Math.max(max , this.map[key])
-    }
+   const val = this.group[this.max].pop();
 
-    for(let i = this.arr.length - 1; i >= 0; i--){
-        const {val } = this.arr[i];
-        console.log(val , this.arr[i])
-        if(this.arr[i].freq === max){
-            this.map[val]--; 
-            this.arr.splice(i,1);
-            return val
-        }
-    }
+   this.map[val]--;
+   if(this.group[this.max].length === 0){
+    this.max --; 
+   }
+   return val;
 };
 
 const freqStack = new FreqStack();
@@ -44,11 +44,8 @@ freqStack.push(5); // The stack is [5,7,5]
 freqStack.push(7); // The stack is [5,7,5,7]
 freqStack.push(4); // The stack is [5,7,5,7,4]
 freqStack.push(5); // The stack is [5,7,5,7,4,5]
-console.log(freqStack.pop())
-console.log(freqStack.pop())
-console.log(freqStack.pop())
-console.log(freqStack.pop())
-console.log(freqStack.pop())
-console.log(freqStack.pop())
 
-
+console.log(freqStack.pop()); // 5
+console.log(freqStack.pop()); // 7
+console.log(freqStack.pop()); // 5
+console.log(freqStack.pop()); // 4
