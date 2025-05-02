71f09635ed3ede8d9b5d6a92c991020859d8e2c8
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-05-02 22:57:21 +0530

    ClearTimout

--- JS/GFE/CancellableTimeout.js
+++ JS/GFE/CancellableTimeout.js
@@ -1,4 +1,4 @@
 export default function setCancellableTimeout(callback, delay, ...args) {
     const Intervals = setTimeout(callback, delay, ...args);
     return () => clearTimeout(Intervals);
-  }
+}
