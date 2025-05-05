79cf10daf8fa9be19aa043463cbd4587dd7d81cb
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-05-06 00:43:30 +0530

    Late night solve

---
+++ DSA/Hash/912.js
@@ -0,0 +1,20 @@
+// var swap = (arr, i, j) => {
+//   let temp = arr[i];
+//   arr[i] = arr[j];
+//   arr[j] = temp;
+// };
+
+// var sortArray = function (nums) {
+//   let st = 0;
+//   let end = nums.length - 1;
+
+//   while (st < end) {
+//     swap(nums, st, end);
+//     st++;
+//     end--;
+//   }
+
+//   return nums;
+// };
+// not checking sorted , no only reverse 
+console.log(sortArray([5, 2, 3, 1]));
