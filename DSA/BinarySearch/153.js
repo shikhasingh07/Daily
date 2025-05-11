6822d72aa7d3a87f94d364773824d9af17e293f5
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-05-12 00:43:52 +0530

    Find Minimum in Rotated Sorted Array

---
+++ DSA/BinarySearch/153.js
@@ -0,0 +1,17 @@
+const rotated = (nums) => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if(nums[mid] > nums[right]){
+        left = mid + 1; 
+    }else {
+        right = mid ;
+    }
+  }
+  return nums[right];
+};
+
+console.log(rotated([3, 4, 5, 1, 2]));
