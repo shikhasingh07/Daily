d82412922679e99d9203e65959a06a1dea783279
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-05-14 23:29:58 +0530

    Notes binary search

--- DSA/BinarySearch/153.js
+++ DSA/BinarySearch/153.js
@@ -11,7 +11,7 @@ const rotated = (nums) => {
         right = mid ;
     }
   }
-  return nums[right];
+  return nums[left];
 };
 
-console.log(rotated([3, 4, 5, 1, 2]));
+console.log(rotated([3, 4, 5, 9, 2]));
