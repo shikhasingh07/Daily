d5933de45c6ea86096b99a402ab28851f0833e2a
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-07-13 23:14:57 +0530

    Floor & Ceil

--- DSA/revision/BinarySearch.js
+++ DSA/revision/BinarySearch.js
@@ -1,33 +1,71 @@
-const lowerBound = () => {
-    let low = 0 ; 
-    let high = nums.length -1;
+// const lowerBound = () => {
+//     let low = 0 ;
+//     let high = nums.length -1;
 
-    while (low <= high){
-        let mid = Math.floor((low + high)/2); 
+//     while (low <= high){
+//         let mid = Math.floor((low + high)/2);
 
-        if(nums[mid] >= n){ 
-            ans = mid ; 
-            high = mid - 1;
-        }else {
-            low = mid + 1;
-        }
+//         if(nums[mid] >= n){
+//             ans = mid ;
+//             high = mid - 1;
+//         }else {
+//             low = mid + 1;
+//         }
+//     }
+// }
+
+// var searchInsert = function(nums, target) {
+
+//     if(nums.length === 0) return 0;
+
+//     let low = 0;
+//     let high = nums.length -1 ;
+//     while (low <= high){
+//         let mid = Math.floor ((low + high)/2);
+
+//         if(nums[mid] >= target){
+//             high = mid - 1;
+//         }else {
+//             low = mid + 1;
+//         }
+//     }
+//     return low;
+// };
+
+const Findceli = (arr, x) => {
+  let low = 0;
+  let high = arr.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (arr[mid] >= x) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
     }
-}
-
-var searchInsert = function(nums, target) {
-    
-    if(nums.length === 0) return 0;
-
-    let low = 0; 
-    let high = nums.length -1 ;
-    while (low <= high){
-        let mid = Math.floor ((low + high)/2); 
-
-        if(nums[mid] >= target){
-            high = mid - 1;
-        }else {
-            low = mid + 1;
-        }
-    }
-    return low; 
+  }
+  return low < arr.length ? arr[low] : -1; // Return -1 if no ceiling exists
 };
+
+const FindFloor = (arr, x) => {
+  let low = 0;
+  let high = arr.length - 1;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+    if (arr[mid] <= x) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return high >= 0 ? arr[high] : -1; // Return -1 if no floor exists
+};
+
+const floorandceli = (arr, x) => {
+  const celi = Findceli(arr, x);
+  const floor = FindFloor(arr , x); 
+  return [celi , floor]
+};
+console.log(floorandceli([3, 4, 4, 7, 8, 10], 8));
--- JS/GFE/Polyfills/Filter.js
+++ JS/GFE/Polyfills/Filter.js
@@ -14,39 +14,42 @@
 // let ans = arr.myFilter((arr,index,value) => value > 3);
 
 //  Promise all 
-Array.prototype.promiseAll =  function () {
-    return new Promise((resolve, reject) => {
-        const result = [];
-        let resolvedCount = 0;
+// Array.prototype.promiseAll =  function () {
+//     return new Promise((resolve, reject) => {
+//         const result = [];
+//         let resolvedCount = 0;
     
-        if (this.length === 0) {
-          resolve(result); // Resolve immediately if the iterable is empty
-        }
+//         if (this.length === 0) {
+//           resolve(result); // Resolve immediately if the iterable is empty
+//         }
     
-        this.forEach((item, index) => {
-          Promise.resolve(item)
-            .then((res) => {
-              result[index] = res; // Store the resolved value at the correct index
-              resolvedCount++; // Increment the resolved count
+//         this.forEach((item, index) => {
+//           Promise.resolve(item)
+//             .then((res) => {
+//               result[index] = res; // Store the resolved value at the correct index
+//               resolvedCount++; // Increment the resolved count
     
-              if (resolvedCount === this.length) {
-                resolve(result); // Resolve when all promises are resolved
-              }
-            })
-            .catch((error) => {
-              reject(error); // Reject immediately if any promise fails
-            });
-        });
-      });
-}
-const promise = [
-    Promise.resolve(1),
-    Promise.resolve(2),
-    Promise.resolve(3),
-]
+//               if (resolvedCount === this.length) {
+//                 resolve(result); // Resolve when all promises are resolved
+//               }
+//             })
+//             .catch((error) => {
+//               reject(error); // Reject immediately if any promise fails
+//             });
+//         });
+//       });
+// }
+// const promise = [
+//     Promise.resolve(1),
+//     Promise.resolve(2),
+//     Promise.resolve(3),
+// ]
 
-promise.promiseAll().then(res => {
-    console.log(res)
-}).catch(err => {
-    console.log(err)
-})
+// promise.promiseAll().then(res => {
+//     console.log(res)
+// }).catch(err => {
+//     console.log(err)
+// })
+
+
+
