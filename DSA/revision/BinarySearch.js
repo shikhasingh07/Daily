e977930459bc1636c49a99a31801e67f61b39e0d
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-07-19 23:28:55 +0530

    Js

--- DSA/revision/BinarySearch.js
+++ DSA/revision/BinarySearch.js
@@ -15,14 +15,11 @@
 // }
 
 // var searchInsert = function(nums, target) {
-
 //     if(nums.length === 0) return 0;
-
 //     let low = 0;
 //     let high = nums.length -1 ;
 //     while (low <= high){
 //         let mid = Math.floor ((low + high)/2);
-
 //         if(nums[mid] >= target){
 //             high = mid - 1;
 //         }else {
@@ -32,40 +29,116 @@
 //     return low;
 // };
 
-const Findceli = (arr, x) => {
+// const Findceli = (arr, x) => {
+//   let low = 0;
+//   let high = arr.length - 1;
+
+//   while (low <= high) {
+//     let mid = Math.floor((low + high) / 2);
+
+//     if (arr[mid] >= x) {
+//       high = mid - 1;
+//     } else {
+//       low = mid + 1;
+//     }
+//   }
+//   return low < arr.length ? arr[low] : -1; // Return -1 if no ceiling exists
+// };
+
+// const FindFloor = (arr, x) => {
+//   let low = 0;
+//   let high = arr.length - 1;
+
+//   while (low <= high) {
+//     let mid = Math.floor((low + high) / 2);
+//     if (arr[mid] <= x) {
+//       low = mid + 1;
+//     } else {
+//       high = mid - 1;
+//     }
+//   }
+//   return high >= 0 ? arr[high] : -1; // Return -1 if no floor exists
+// };
+
+// const floorandceli = (arr, x) => {
+//   const celi = Findceli(arr, x);
+//   const floor = FindFloor(arr , x);
+//   return [celi , floor]
+// };
+// console.log(floorandceli([3, 4, 4, 7, 8, 10], 8));
+
+// Last occurrence in a sorted array
+// const lastIndex = (arr , tar) => {
+
+//     let low = 0 ;
+//     let high = arr.length - 1 ;
+
+//     while(low <= high){
+//         let mid = Math.floor((low + high) / 2);
+
+//         if(arr[mid] <= tar){
+//             low = mid + 1 ;
+//         }else {
+//             high = mid - 1;
+//         }
+//     }
+//     return high >= 0 ? high : -1;;
+// }
+// console.log(lastIndex([3, 4, 8, 8, 8, 10], 8));
+
+// [7 , 8 , 9 , 1, 2 ,3 ,4,5] 8
+// const searchElementInRotated = (arr, tar) => {
+//   let low = 0;
+//   let high = arr.length - 1;
+//   let ans = -1;
+//   while (low <= high) {
+//     let mid = Math.floor((low + high) / 2);
+
+//     if (arr[mid] === tar) {
+//       ans = mid;
+//       break;
+//     }
+
+//     if (arr[low] <= tar) {
+//       // left
+//       if (arr[low] <= tar && tar <= arr[mid]) {
+//         high = mid + 1;
+//       } else {
+//         low = mid - 1;
+//       }
+//     } else {
+//       // right
+//       if (arr[mid] <= tar && tar <= arr[high]) {
+//         low = mid - 1;
+//       } else {
+//         high = mid + 1;
+//       }
+//     }
+//   }
+//   return ans;
+// };
+
+// main thing is to - smalling the window
+const searchElementInRotatedDuplicate = (arr, k) => {
   let low = 0;
   let high = arr.length - 1;
 
   while (low <= high) {
     let mid = Math.floor((low + high) / 2);
 
-    if (arr[mid] >= x) {
-      high = mid - 1;
-    } else {
-      low = mid + 1;
+    if (arr[mid] === k) {
+      return mid;
     }
-  }
-  return low < arr.length ? arr[low] : -1; // Return -1 if no ceiling exists
-};
 
-const FindFloor = (arr, x) => {
-  let low = 0;
-  let high = arr.length - 1;
-
-  while (low <= high) {
-    let mid = Math.floor((low + high) / 2);
-    if (arr[mid] <= x) {
-      low = mid + 1;
-    } else {
-      high = mid - 1;
+    // now smalling window
+    if (arr[low] <= arr[mid] && arr[mid] <= arr[right]) {
+      low++;
+      high--;
+      continue;
     }
+
+    // same as searchElementInRotated
   }
-  return high >= 0 ? arr[high] : -1; // Return -1 if no floor exists
 };
 
-const floorandceli = (arr, x) => {
-  const celi = Findceli(arr, x);
-  const floor = FindFloor(arr , x); 
-  return [celi , floor]
-};
-console.log(floorandceli([3, 4, 4, 7, 8, 10], 8));
+console.log(searchElementInRotated([3, 4, 8, 8, 8, 10], 8));
--- JS/GFE/Polyfills/Filter.js
+++ JS/GFE/Polyfills/Filter.js
@@ -1,34 +1,34 @@
 // Filters
 // Array.prototype.myFilter = function (callback){
-//   let ans = []; 
+//   let ans = [];
 
 //   for(let i = 0 ; i < this.length ; i++){
 //     if(callback(this , i , this[i])){
-//         ans.push(this[i]); 
+//         ans.push(this[i]);
 //     }
 //   }
-//   return ans ; 
+//   return ans ;
 // }
 
 // let arr = [3,6,9,12,15,18];
 // let ans = arr.myFilter((arr,index,value) => value > 3);
 
-//  Promise all 
+//  Promise all
 // Array.prototype.promiseAll =  function () {
 //     return new Promise((resolve, reject) => {
 //         const result = [];
 //         let resolvedCount = 0;
-    
+
 //         if (this.length === 0) {
 //           resolve(result); // Resolve immediately if the iterable is empty
 //         }
-    
+
 //         this.forEach((item, index) => {
 //           Promise.resolve(item)
 //             .then((res) => {
 //               result[index] = res; // Store the resolved value at the correct index
 //               resolvedCount++; // Increment the resolved count
-    
+
 //               if (resolvedCount === this.length) {
 //                 resolve(result); // Resolve when all promises are resolved
 //               }
@@ -39,6 +39,7 @@
 //         });
 //       });
 // }
+
 // const promise = [
 //     Promise.resolve(1),
 //     Promise.resolve(2),
@@ -51,5 +52,33 @@
 //     console.log(err)
 // })
 
+//debounce
+// function debounce(func, wait) {
+//   let timeout;
+//   return function (...args) {
+//     const context = this;
+//     clearTimeout(timeout);
+//     timeout = setTimeout(() => {
+//       func.apply(context, args);
+//     }, wait);
+//   };
+// }
 
+// Flatten
+// function Flatten (arg){
+//     let ans = []
+//     for(let item of arg){
+//         if(Array.isArray(item)){
+//            ans.push(...Flatten(item))
+//         }else{
+//             ans.push(item)
+//         }
+//     }
+//     return ans; 
+// };
+
+//curry 
+function curry () {
+
+}
 
