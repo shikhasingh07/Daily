c8a923dce0de6ced83de2980154988ea5db3e037
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-08-14 21:53:58 +0530

    questions

--- DSA/TwoPointer/633.js
+++ DSA/TwoPointer/633.js
@@ -1,21 +1,29 @@
-const squars = (c) =>{
+// const squars = (c) =>{
 
-    let left = 0 ;
-    let right = Math.floor(Math.sqrt(c)); 
+//     let left = 0 ;
+//     let right = Math.floor(Math.sqrt(c)); 
 
-    while (left <= right){
-        let sum = Math.pow(left, 2) + Math.pow(right, 2);
-        if(sum === c){
-            return true; 
-        }
-        else if(sum > c){
-            right --; 
-        }else {
-            left ++ ; 
-        }
+//     while (left <= right){
+//         let sum = Math.pow(left, 2) + Math.pow(right, 2);
+//         if(sum === c){
+//             return true; 
+//         }
+//         else if(sum > c){
+//             right --; 
+//         }else {
+//             left ++ ; 
+//         }
 
-    }
-    return false;
+//     }
+//     return false;
+// }
+
+// console.log(squars(5))
+
+
+// how to find all the pairs in array 
+const paris = function (nums) {
+
 }
 
-console.log(squars(5))
+console.log(paris([1,3,1] , k))
