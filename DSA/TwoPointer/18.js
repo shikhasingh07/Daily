089a3f69a77f0bf18802060403854b0b04390131
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-08-16 00:51:42 +0530

    revision

--- DSA/TwoPointer/18.js
+++ DSA/TwoPointer/18.js
@@ -37,10 +37,37 @@
 //   return ans;
 // };
 
+var fourSum = function (nums, target) {
+  nums.sort((a, b) => a - b);
+  let ans = [];
 
-// var fourSum = function (nums, target) {
-//   nums.sort((a, b) => a - b);
-//   // [-2 , -1 , 0 , 1 , 2]
-// };
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    for (let j = i + 1; j < nums.length - 2; j++){
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue;
 
-// console.log(fourSum([1, 0, -1, 0, -2, 2], 8));
+      let left = j + 1;
+      let right = nums.length - 1;
+
+      while (left < right) {
+        let sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+        if (sum === target) {
+          ans.push([nums[i], nums[j], nums[left], nums[right]]);
+
+          while (left < right && nums[left] === nums[left + 1]) left++;
+          while (left < right && nums[right] === nums[right - 1]) right--;
+          left++;
+          right--;
+        } else if (sum < target) {
+          left++;
+        } else {
+          right--;
+        }
+      }
+    }
+  }
+  return ans;
+};
+
+console.log(fourSum([1, 0, -1, 0, -2, 2], 0));
