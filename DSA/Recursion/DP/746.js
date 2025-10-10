20e49a50d565d071d75bd3618cfd33a25c5217e9
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-10-11 01:07:09 +0530

    House Robber

--- DSA/Recursion/DP/746.js
+++ DSA/Recursion/DP/746.js
@@ -1,16 +1,14 @@
-function dp(n, cost, arr) {
+var cost = [10, 15, 20]; 
+function dp(n) {
   if (n <= 1) return 0;
 
-  if (arr[n] !== undefined) return arr[n];
-
-  arr[n] = Math.min(
-    dp(n - 1, cost, arr) + cost[n - 1],
-    dp(n - 2, cost, arr) + cost[n - 2]
-  );
-  return arr[n];
+  return Math.min(
+    dp(n - 1) + cost[n - 1],
+    dp(n - 2) + cost[n - 2]
+  )
 }
 var minCostClimbingStairs = function (cost) {
-  return dp(cost.length, cost, new Array(cost.length + 1));
+  return dp(cost.length);
 };
 
 console.log(minCostClimbingStairs([10, 15, 20]));
