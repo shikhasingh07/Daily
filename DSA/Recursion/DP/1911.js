53b43dc850e46ccc65b88dd47a849de9a965ab4b
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-10-14 20:53:48 +0530

    DP

--- DSA/Recursion/DP/1911.js
+++ DSA/Recursion/DP/1911.js
@@ -1,21 +1,3 @@
-function backtracking(nums, idx, parity, memo) {
-  if (nums.length === idx) {
-    return 0;
-  }
-  if (!memo[idx]) memo[idx] = [];
-  if (memo[idx][parity] !== undefined) {
-    return memo[idx][parity];
-  }
-
-  let skip = backtracking(nums, idx + 1, parity, memo);
-  const val = parity === 0 ? +nums[idx] : -nums[idx];
-  const take = val + backtracking(nums, idx + 1, 1 - parity, memo);
-
-  memo[idx][parity] = Math.max(skip, take);
-  return memo[idx][parity];
-}
-var maxAlternatingSum = function (nums) {
-  return backtracking(nums, 0, 0, new Array(nums.length));
-};
+ 
 
 console.log(maxAlternatingSum([4, 2, 5, 3]));
