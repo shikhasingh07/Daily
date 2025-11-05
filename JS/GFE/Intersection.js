c074fac2bc4d6306972f3dd9647808ce05cf3566
shikhasingh07 <shikha.thakur2295@gmail.com>
2025-11-05 23:43:14 +0530

    inertsection UI

--- JS/GFE/Intersection.js
+++ JS/GFE/Intersection.js
@@ -1,9 +1,13 @@
 function intersection(...arrays) {
-  throw 'Not implemented!';
+  if (arrays.length === 0) return [];
+
+  return [...new Set(arrays[0])].filter((value) =>
+    arrays.every((arr) => arr.includes(value))
+  );
 }
 
 const arr1 = [1, 2, 3];
 const arr2 = [2, 3, 4];
 const arr3 = [3, 4, 5];
 
-console.log(intersection(arr1, arr2, arr3))
+console.log(intersection(arr1, arr2, arr3));
