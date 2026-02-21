cb84339b0b962dbbdc7ac9aac794e6a0ffae0887
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-02-21 21:10:07 +0530

    learned

---
+++ Graph/Traversal/785.js
@@ -0,0 +1,34 @@
+function dfs(i, graph, color) {
+  for (let neigh of graph[i]) {
+    if (color[neigh] === -1) {
+      color[neigh] = 1 - color[i];
+
+      if (!dfs(neigh, graph, color)) {
+        return false;
+      }
+    } else if (color[neigh] === color[i]) {
+      return false;
+    }
+  }
+}
+var isBipartite = function (graph) {
+  let n = graph.length;
+  let color = new Array(n).fill(-1);
+
+  for (let i = 0; i < n; i++) {
+    if (color[i] === -1) {
+      color[i] = 0;
+      if (!dfs(i, graph, color)) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+let graph = [
+  [1, 2, 3],
+  [0, 2],
+  [0, 1, 3],
+  [0, 2],
+];
+console.log(isBipartite(graph));
