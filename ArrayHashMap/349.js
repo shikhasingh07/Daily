614eadfd29ab13b5ea5017f463f62aa69ba56170
shikhasingh07 <shikha.thakur2295@gmail.com>
2026-04-05 20:19:21 +0530

    intersection

--- ArrayHashMap/349.js
+++ ArrayHashMap/349.js
@@ -1,15 +1,17 @@
 var intersection = function (nums1, nums2) {
-  let set = new Set(nums1);
-
-  let ans = new Set();
-
-  for (let num of nums2) {
-    if (set.has(num)) {
-      ans.add(num);
-    }
+  let map = new Map();
+  let ans = [];
+  for (let i of nums1) {
+    map.set(i, (map.get(i) || 0) + 1);
   }
 
-  return [...ans];
+  for (let num of nums2) {
+    if (map.has(num) && map.get(num) > 0) {
+      ans.push(num);
+      map.set(num, (map.get(num) || 0) - 1);
+    }
+  }
+  return ans;
 };
 let nums1 = [1, 2, 2, 1];
 let nums2 = [2, 2];
